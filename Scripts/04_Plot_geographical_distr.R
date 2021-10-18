5b4b6260410a10e04c5d13396da9294886ba8628
Johannes Radinger <johannesradinger@gmail.com>
2021-10-18 08:34:31 +0200

    improvement and remake of map figure

--- Scripts/04_Plot_geographical_distr.R
+++ Scripts/04_Plot_geographical_distr.R
@@ -7,14 +7,21 @@
 
 
 library(ggplot2)
+library(scales)
 library(ggsci) #https://cran.r-project.org/web/packages/ggsci/vignettes/ggsci.html
 library(xlsx)
 library(dplyr)
 library(plyr)
 library(see)
 library(rgdal)
+library(gtable)
+library(gridExtra)
 library(sp)
+library(ggspatial)
+library(patchwork)
 #devtools::install_github("ropenscilabs/rnaturalearth")
+devtools::install_github("burchill/zplyr")
+library(zplyr)
 library("rnaturalearth")
 library("rnaturalearthdata")
 
@@ -66,55 +73,76 @@ europe <- ne_countries(scale = "medium", returnclass = "sf",
 world <- ne_countries(scale = "medium", returnclass = "sf")
 
 
-### Plot maps for contients
+### Plot maps for continents
 Europe <- ggplot(data = world) +
   geom_sf(fill="grey70",color="grey90",size=0.3) +
   geom_point(data=summary_df, aes(x=Long,y=Lat,
-                                  size=capacity_class_numeric),
+                                  size=capacity_class_numeric,
+                                  fill=capacity_class),
              shape=21,
-             fill=material_colors("deep purple"),
+             #fill=material_colors("deep purple"),
              color="white",
-             alpha=0.6)+
-  scale_radius(name="Hydropower scale", range = c(3, 10),breaks=c(1,2,3),labels=c("vSHP","SHP","LHP"))+
-  scale_y_continuous(name="")+
-  scale_x_continuous(name="")+
-  coord_sf(xlim = c(-13, 33), ylim = c(40, 65), expand = FALSE,datum = NA)+
+             alpha=0.7)+
+  scale_fill_manual(values=c("#ffcd38","#cb9904","#997304"))+
+  scale_radius(name="Hydropower scale", range = c(3, 9),breaks=c(1,2,3))+
+  coord_sf(xlim = c(-12, 33), ylim = c(40, 65), expand = T,datum = NA)+
   theme_minimal()+
+  annotation_scale(location = "br", width_hint = 0.5,
+                   bar_cols = c("grey40", "grey90"),
+                   line_width = 0,
+                   height=unit(0.15,"cm")) +
+  geom_abs_text(aes(xpos=0.05,ypos=0.95,label="b"),size=5.5,data = data.frame())+
+  theme(axis.title=element_blank(),
+        legend.position = "none",
+        legend.justification = c(0, 0),
+        legend.box.background=element_rect(fill="white",colour="grey50"),
+        panel.border = element_rect(colour = "grey50", fill=NA),
+        plot.background = element_rect(colour = "grey50", fill=NA),
+        plot.margin = unit(c(0, 0, 0, 0), "null"))+
   guides(shape = guide_legend(ncol = 1, override.aes = list(size = 3, color = material_colors("deep purple"))),
-         size = guide_legend(override.aes = list(color = material_colors("deep purple"))))
+         size = guide_legend(override.aes = list(fill=c("#cb9904","#ffcd38","#997304"))),
+         fill = "none")
 
 NorthAmerica <- ggplot(data = world) +
   geom_sf(fill="grey70",color="grey90",size=0.3) +
   geom_point(data=summary_df, aes(x=Long,y=Lat,
-                                  size=capacity_class_numeric),
+                                  size=capacity_class_numeric,
+                                  fill=capacity_class),
              shape=21,
-             fill=material_colors("deep purple"),
+             #fill=material_colors("deep purple"),
              color="white",
-             alpha=0.6)+
-  scale_radius(name="Hydropower scale", range = c(3, 10),breaks=c(1,2,3),labels=c("vSHP","SHP","LHP"))+
-  scale_y_continuous(name="")+
-  scale_x_continuous(name="")+
-  coord_sf(xlim = c(-140, -30), ylim = c(25, 65), expand = FALSE,datum = NA)+
+             alpha=0.7)+
+  #scale_fill_manual(values=c("#ffcd38","#997304","#cb9904"))+
+  scale_fill_manual(values=c("#ffcd38","#cb9904","#997304"))+
+  scale_radius(name="Hydropower scale", range = c(3, 9),breaks=c(1,2,3),
+               labels=c(expression(""<1~'MW'),
+                        expression(1-10~'MW'),
+                        expression("">=10~'MW')))+
+  #annotate(geom = 'text', label = 'a', x = -Inf, y = Inf, hjust = 0.1, vjust = 0.95)+
+  geom_abs_text(aes(xpos=0.05,ypos=0.95,label="a"),size=5.5,data = data.frame())+
+  coord_sf(xlim = c(-127, -50), ylim = c(17, 66.5), expand = T, datum = NA,clip="on")+
   theme_minimal()+
+  annotation_scale(location = "br", width_hint = 0.5,
+                   bar_cols = c("grey40", "grey90"),
+                   line_width = 0,
+                   height=unit(0.15,"cm")) +
+  theme(axis.title=element_blank(),
+        legend.title = element_blank(),
+        legend.position = c(0, 0),
+        legend.justification = c(0, 0),
+        legend.background = element_rect(fill=alpha('white',0.75),colour="grey50"),
+        panel.border = element_rect(colour = "grey50", fill=NA),
+        plot.background = element_rect(colour = "grey50", fill=NA),
+        plot.margin = unit(c(0, 0, 0, 0), "null"))+
   guides(shape = guide_legend(ncol = 1, override.aes = list(size = 3, color = material_colors("deep purple"))),
-         size = guide_legend(override.aes = list(color = material_colors("deep purple"))))
-
-Oceanea <- ggplot(data = world) +
-  geom_sf(fill="grey70",color="grey90",size=0.3) +
-  geom_point(data=summary_df, aes(x=Long,y=Lat,
-                                  size=capacity_class_numeric),
-             shape=21,
-             fill=material_colors("deep purple"),
-             color="white",
-             alpha=0.6)+
-  scale_radius(name="Hydropower scale", range = c(3, 10),breaks=c(1,2,3),labels=c("vSHP","SHP","LHP"))+
-  scale_y_continuous(name="")+
-  scale_x_continuous(name="")+
-  coord_sf(xlim = c(-13, 33), ylim = c(40, 65), expand = FALSE,datum = NA)+
-  theme_minimal()+
-  guides(shape = guide_legend(ncol = 1, override.aes = list(size = 3, color = material_colors("deep purple"))),
-         size = guide_legend(override.aes = list(color = material_colors("deep purple"))))
+         size = guide_legend(override.aes = list(fill=c("#cb9904","#ffcd38","#997304"))),
+         fill = "none")
 
 
-cairo_pdf("./Results_Figs_Tabs/Map_turbines.pdf",width=6,height=6)
+NorthAmerica + Europe
+
+
+
+cairo_pdf("./Results_Figs_Tabs/Map_turbines.pdf",width=9,height=4.5)
+NorthAmerica + Europe
 dev.off()
